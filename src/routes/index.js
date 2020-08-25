f378874e9a9b0fab1c1f97023d0f0798c451911d
Jorge <jhessle04@gmail.com>
2020-08-25 14:56:49 -0300

    update

---
+++ src/routes/index.js
@@ -0,0 +1,149 @@
+const express = require('express');
+const router = express.Router();
+
+const Post = require('../models/post');
+const Comment = require('../models/comment');
+
+
+
+
+
+
+
+
+
+router.get('/', async (req, res) => {
+  res.render('portada')
+});
+
+
+router.get('/allposts', async (req, res) => {
+  const post = await Post
+  .find()
+  .sort({ timestamp: -1 });
+  const comments = await Comment
+  .find({post_id: post._id})
+  .sort({ timestamp: -1 });
+  res.render('allposts', {
+    post, comments
+  });
+});
+
+   
+
+router.post('/add', async (req, res) => {
+  const task = new Post(req.body);
+  await task.save();
+  res.redirect('/indexbackend/1');
+});
+
+
+
+router.get('/postredirect/:post_id', async (req, res) => {
+    const { post_id } = req.params;
+    const post = await Post.findById(post_id);
+    const count = await Comment.countDocuments(post_id);
+    const comments = await Comment.
+    find({post_id: post._id})
+    .sort({ timestamp: -1 });
+
+    //console.log(post);  167.71.252.38
+    //res.send('recibido');
+     res.render('postredirect', {post, comments, count});
+  });
+
+
+  router.get('/postbackend/:post_id', async (req, res) => {
+    const { post_id } = req.params;
+    const post = await Post.findById(post_id);
+    const comments = await Comment.find({post_id: post._id});
+    //console.log(post);
+    //res.send('recibido');
+     res.render('postbackend', {post, comments});
+  });  
+
+
+
+
+  router.post('/post/:post_id/comment', async (req, res, next) => {
+    const post = await Post.findById(req.params.post_id);
+    if (post) {
+      const newComment = new Comment(req.body);
+      newComment.post_id = post._id
+      //console.log(newComment);  
+      await newComment.save();
+      res.redirect('/postRedirect/' + post._id + '#' + newComment._id )
+    }
+    //console.log(newComment);  
+    //console.log(req.params.post_id);  
+  });
+    
+  
+
+  router.get('/index/:page',   async (req, res) => {
+
+    let perPage = 9;
+    let page = req.params.page || 1;
+  
+    Post
+      .find({}) // finding all documents
+      .sort({ timestamp: -1 })
+      .skip((perPage * page) - perPage) // in the first page the value of the skip is 0
+      .limit(perPage) // output just 9 items
+      .exec((err, post) => {
+        Post.countDocuments((err, count) => { // count to calculate the number of pages
+          if (err) return next(err);
+          res.render('index', {
+            post,
+            current: page,
+            pages: Math.ceil(count / perPage)
+          });
+        });
+      });
+  });
+
+
+
+
+router.get('/indexbackend/:page',   async (req, res) => {
+    let perPage = 9;
+    let page = req.params.page || 1;
+  
+    Post
+      .find({}) // finding all documents
+      .sort({ timestamp: -1 })
+      .skip((perPage * page) - perPage) // in the first page the value of the skip is 0
+      .limit(perPage) // output just 9 items
+      .exec((err, post) => {
+        Post.countDocuments((err, count) => { // count to calculate the number of pages
+          if (err) return next(err);
+          res.render('indexbackend', {
+            post,
+            current: page,
+            pages: Math.ceil(count / perPage)
+          });
+        });
+      });
+  });
+
+
+
+  router.get('/delete/:id', async (req, res, next) => {
+    const { id } = req.params;
+    await Post.remove({_id: id});
+    res.redirect('/indexbackend/1');
+  });
+
+
+  router.get('/delete/comment/:id', async (req, res, next) => {
+    const { id } = req.params;
+    await Comment.deleteOne({_id: id});
+    res.redirect('/indexbackend/1');
+  });
+
+ 
+
+
+
+
+module.exports = router;
---
+++ src/server.js
@@ -0,0 +1,52 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+  } 
+
+
+const path = require('path');
+const morgan = require('morgan');
+const express = require('express');
+const { format } = require('timeago.js');
+const engine = require('ejs-mate');
+
+
+const app = express();
+
+//bbdd
+const { mongoose } = require('./database');
+
+//bbddmongodb://localhost/estudio-db
+//mongoose.connect('mongodb://localhost/estudio-db')
+//.then(db => console.log('Db connected'))
+//.catch(err => console.log(err));
+
+
+//importing routes
+const indexRoutes = require('./routes/index');
+
+//settings
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('ejs', engine);
+app.set('view engine', 'ejs');
+
+//middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+
+app.use(express.static(path.join(__dirname, 'views')));
+
+// Global variables
+app.use((req, res, next) => {
+app.locals.format = format;
+next();
+});
+
+//routes
+app.use('/', indexRoutes);
+
+
+// server
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`);
+});
